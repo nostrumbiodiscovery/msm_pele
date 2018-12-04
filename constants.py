b7b7edf5520ee9a4ffd2b13f61524d74180c62d8
daniel <daniel.soler@e-campus.uab.cat>
2018-12-04 09:29:13 +0100

    Change MN4 path to new PELE binary

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ import os
 machine = socket.getfqdn()
 if "bsc.mn" in machine:
     SCHRODINGER = "/gpfs/projects/bsc72/SCHRODINGER_ACADEMIC"
-    PELE = "/gpfs/projects/bsc72/PELE++/nord/rev090518"
-    PELE_BIN = "/gpfs/projects/bsc72/PELE++/nord/rev090518/bin/PELE-1.5_mpi"
+    PELE = "/gpfs/projects/bsc72/MultiBoxPELE/bin/"
+    PELE_BIN = "/gpfs/projects/bsc72/MultiBoxPELE/build/PELE-1.5"
     MPIRUN = "/apps/INTEL/2017.4/impi/2017.3.196/bin64"
     LICENSE = "/gpfs/projects/bsc72/PELE++/license"
     MMSHARE = None
@@ -24,7 +24,6 @@ elif "mn.bsc" in machine:
     MPIRUN = "/apps/OPENMPI/1.8.1-mellanox/bin"
     LICENSE = "/gpfs/projects/bsc72/PELE++/license"
     MMSHARE = None
-    PYTHON = "python"
     # Provisional workaround until best_struct.py is fixed
     ACCEPTED_STEPS_NAME = "AcceptedSteps"
     CRITERIA = "SASA"
@@ -34,9 +33,11 @@ elif "bsccv" in machine:
     PELE = "/data/EAPM/PELE/PELE++/life/rev12489"
     PELE_BIN = "/data/EAPM/PELE/PELE++/life/rev12489/bin/PELE-1.5_mpi"
     MPIRUN = "/data2/apps/OPENMPI/1.6.1/bin"
+    #LICENSE = "/gpfs/projects/bsc72/PELE++/license"
     MMSHARE = None
     LICENSE = "/data/EAPM/PELE/PELE++/license"
-    PYTHON = "/data2/apps/PYTHON/2.7.5/bin/python2.7"
+    # MMSHARE = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.9/bin/Linux-x86_64"
+    # SCHRODINGER_PYTHON_LIBS = "/data2/bsc72/SCHRODINGER_ACADEMIC/mmshare-v3.9/lib/Linux-x86_64/lib/python2.7/site-packages/"
     # Provisional workaround until best_struct.py is fixed
     ACCEPTED_STEPS_NAME = "AcceptedSteps"
     CRITERIA = "SASA"
@@ -95,6 +96,31 @@ NATIVE = '''
                         }},\n\
 
 
+'''
+
+BOX = '''
+MODEL $MODEL
+HEADER    CORNERS OF BOX
+REMARK    CENTER (X Y Z)   $CENTER_X  $CENTER_Y  $CENTER_Z
+REMARK    RADIUS $RADIUS
+REMARK    DIMENSIONS (X Y Z)   31.116  26.557  29.958
+ATOM      1  DUA BOX     1 $V1
+ATOM      2  DUB BOX     1 $V2
+ATOM      3  DUC BOX     1 $V3
+ATOM      4  DUD BOX     1 $V4
+ATOM      5  DUE BOX     1 $V5
+ATOM      6  DUF BOX     1 $V6
+ATOM      7  DUG BOX     1 $V7
+ATOM      8  DUH BOX     1 $V8
+CONECT    1    2    4    5
+CONECT    2    1    3    6
+CONECT    3    2    4    7
+CONECT    4    1    3    8
+CONECT    5    1    6    8
+CONECT    6    2    5    7
+CONECT    7    3    6    8
+CONECT    8    4    5    7
+ENDMDL
 '''
 
 SYSTEM = "System {} checked successfully\n\t**Missing residues found {}\n\t**Gaps found {}\n\t**Metals found {}"
