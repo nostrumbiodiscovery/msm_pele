ee21b62f7e029973171e6ea4a16ec15233fb2382
daniel <daniel.soler@e-campus.uab.cat>
2019-02-04 15:31:00 +0100

    Optimize multiple boxes automatically

--- Box/multiple_box.py
+++ Box/multiple_box.py
@@ -1,4 +1,5 @@
 import numpy as np
+import prody as pd
 import MSM_PELE.Box.box_helpers as mbp
 
 
@@ -11,31 +12,32 @@ def build_box(cluster_centers, env):
     centroid = mbp.find_centroid(cluster_centers)
 
     # Get center and radius along the exit path
-    center, radius = get_centers(BS_location, centroid)
+    center, radius = get_centers(env.adap_ex_input, BS_location, centroid)
 
     return center, radius, "multiple"
 
-def get_centers(BS_location, centroid, radius=5):
+def get_centers(system, BS_location, centroid, radius=9):
     # Equation to get the radius of one sphere if we want
     # to fit n spheres in a segment of distance d
     # radius=d/(2n-2)
     current_point = BS_location
-    final_point = BS_location + 2*(centroid-BS_location)
-    distance = np.linalg.norm(final_point-current_point)
-    distance_centroid = np.linalg.norm(final_point-centroid)
-    number_spheres = int(distance/(2*radius)+1) if int(distance/(2*radius)+1) > 1 else 2
-    radius = distance/(2*number_spheres-2)
     exit_func = lambda x: BS_location + x * (centroid-BS_location)/np.linalg.norm(centroid-BS_location)
-    radius = radius if 100 > radius > 4 else 5
-    alpha = radius
-    centers = []
+    radius = radius if 100 > radius > 5 else 9
+    alpha = 0
     # Check the distance to the centroid because we want to pass the centroid
     # and go two times that distance.
-    while np.linalg.norm(centroid-current_point) <= 2*distance_centroid:
+    centers = []
+    while find_contacts(system, current_point):
         current_point = exit_func(alpha)
         centers.append(current_point)
         alpha += radius
         print("Point {}".format(current_point))
-        print("Next distance {}".format(np.linalg.norm(centroid-current_point)))
+        print("Next distance {}".format(np.linalg.norm(BS_location-current_point)))
     radius = [radius]*len(centers)
     return centers, radius
+
+def find_contacts(system, point):
+    atoms = pd.parsePDB(system)
+    contacts = pd.measure.Contacts(atoms)
+    return contacts.select(5, point) 
+
